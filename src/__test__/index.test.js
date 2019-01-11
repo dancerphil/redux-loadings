66e605af54fb7b8d21de92e93adfa1b99214a206
Cong Zhang <dancerphil1994@gmail.com>
2019-01-11 20:10:15 +0800

    0.6.2

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,9 +1,23 @@
-import { set, load, connectWith } from '..';
+import {
+  set, load, connect, connectWith, Region, Provider, getProvider, reducer,
+  setConfig, getLoading, getResults, getFetchTimes, getError, mapResultToProps,
+} from '../index';
 
 describe('exports', () => {
   test('exports', () => {
     expect(typeof set).toBe('function');
     expect(typeof load).toBe('function');
+    expect(typeof connect).toBe('function');
     expect(typeof connectWith).toBe('function');
+    expect(typeof Region).toBe('function');
+    expect(typeof Provider).toBe('function');
+    expect(typeof getProvider).toBe('function');
+    expect(typeof reducer).toBe('function');
+    expect(typeof setConfig).toBe('function');
+    expect(typeof getLoading).toBe('function');
+    expect(typeof getResults).toBe('function');
+    expect(typeof getFetchTimes).toBe('function');
+    expect(typeof getError).toBe('function');
+    expect(typeof mapResultToProps).toBe('function');
   });
 });
